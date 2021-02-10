453d234016d24e869fbdb03d49787d118ebd4661
patrickrnelson <patrickrnelson1@gmail.com>
2021-02-09 18:56:10 -0600

    transferred SQL from postico to the database file

--- database.sql
+++ database.sql
@@ -1,5 +1,23 @@
-CREATE TABLE "artists" (
-    "id" SERIAL PRIMARY KEY,
-    "artist_name" varchar(80) not null,
-    "year_born" date
+CREATE TABLE artists (
+    "id" serial PRIMARY KEY,
+    "name" varchar(80) NOT NULL,
+    "birthday" DATE
 );
+
+CREATE TABLE songs (
+    "id" serial PRIMARY KEY,
+    "title" varchar(255) NOT NULL,
+    "length" varchar(10),
+    "release" DATE
+);
+
+INSERT INTO "artists" ("name", "birthday")
+VALUES ('Ella Fitzgerald', '04-25-1917'), 
+('Dave Brubeck', '12-06-1920'),
+('Miles Davis', '05-26-1926'),
+('Esperanza Spalding', '10-18-1984');
+
+INSERT INTO "songs" ("title", "length", "release")
+VALUES ('Take Five', '5:24', '1959-09-29'),
+('So What', '9:22', '1959-08-17'),
+('Black Gold', '5:17', '2012-02-01');
