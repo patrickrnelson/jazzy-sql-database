544f1fec06ef6a1d32d3edc27a511e6ed55bb1a4
patrickrnelson <patrickrnelson1@gmail.com>
2021-02-09 20:50:07 -0600

    All stretch goals are functioning

--- server/server.js
+++ server/server.js
@@ -15,50 +15,5 @@ app.listen(PORT, () => {
 let artistRouter = require('./routes/artist_router');
 app.use('/artist', artistRouter);
 
-app.get('/song', (req, res) => {
-  console.log(`In /songs GET`);
-  pool
-    .query(
-      `
-    SELECT * FROM "songs"
-    ORDER BY "title" ASC;
-    `
-    )
-    .then(function (dbRes) {
-      res.send(dbRes.rows);
-    })
-    .catch(function (err) {
-      console.log(err);
-      res.sendStatus(500);
-    });
-});
-
-/* 
-/songs POST should look like....
-
-INSERT INTO "songs"
-  ("title", "length", "release")
-VALUES 
-  ('SOME SONG', 'SOME LENGTH', 'SOME DATE');
-
-*/
-
-app.post('/song', (req, res) => {
-  // console.log('req.body', req.body);
-  pool
-    .query(
-      `
-    INSERT INTO "songs"
-      ("title", "length", "release")
-    VALUES
-      ('${req.body.title}', '${req.body.length}', '${req.body.released}');
-  `
-    )
-    .then(function (dbRes) {
-      res.sendStatus(201);
-    })
-    .catch(function (err) {
-      console.log(err);
-      res.sendStatus(500);
-    });
-});
+let songRouter = require('./routes/song_router');
+app.use('/song', songRouter);
